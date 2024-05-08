51f8f9a93207199b4ed3b7a2783089f1ef12cc12
Chris Smith <chrissmith@mount7.com>
2024-05-08 15:49:11 +0200

    init commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# Camera Pi
+
+This repository allows for privacy masks to be enabled and disabled. It is intended to be connected to a security 
+system or some system that can provide an "armed" state and an "alarm" state.
+
+## Environment Setup and Pi Setup
+
+With a Raspberry Pi and a base OS perform the following steps to deploy the code. You should be able to use any 
+Raspberry Pi. In the next steps you will clone the git repository, setup your Python virtual environment, 
+download the required packages from PyPi, and then install the service. This assumes you are currently in the 
+user's home directory and with a subfolder of `security-camera-privacy-mask`
+
+1. `git clone git@bitbucket.org:mount7freiburg/security-camera-privacy-mask.git`
+2. `cd security-camera-privacy-mask`
+3. `python -m venv .venv`
+4. `.venv/bin pip install -r requirements.txt`
+5. `cp .env.example .env`
+6. `cp cameras.json cameras.json`
+7. Make appropriate changes to `.env` file
+8. Make appropriate changes to `cameras.json` file
+9. Connect wires to GPIO 16 and GPIO 20 for the Raspberry Pi
+
+## Service Setup
+
+1. `cp camerapi.service /etc/systemd/system/camerapi.service`
+2. `sudo systemctl daemon-reload`
+3. `sudo systemctl enable camerapi.service`
+
+## Understanding the env vars and cameras.json file
+
+I am using [Postmark App](https://postmarkapp.com/) for sending emails. You can use any service you want. If you use
+a different service you will need to probably configure a different client or SMTP service.
+
+The `.env` file also contains `LOGFILE_PATH` should be the full path to your log file. `LOG_DEBUG` can be any integer
+that is typically used for Python logging levels (0, 10, 20, 30... etc). `CAMERA_USERNAME` and `CAMERA_PASSWORD` needs
+to be a user that is allowed to make configuration changes. The camera API calls are programmed to call Dahua cameras.
+This should also work with Dahua whitelabeled cameras.
+
+The `cameras.json` file contains **channel**, **type**, and **ip**. Only **ip** and **type** are required. The **type**
+field can either be `exterior` or `interior`. Channel doesn't do anything but can be used as a reference for you.
---
+++ security-system-check.py
@@ -0,0 +1,121 @@
+import time
+from datetime import datetime
+import logging
+import RPi.GPIO as GPIO
+import json
+
+from dotenv import load_dotenv
+from postmarker.core import PostmarkClient
+from requests.auth import HTTPDigestAuth
+import requests
+
+# Read camera data from JSON file
+with open('cameras.json', 'r') as file:
+    camera_data = json.load(file)
+
+# Get interior cameras
+interior_cameras = [camera['ip'] for camera in camera_data if camera['type'] == 'interior']
+exterior_cameras = [camera['ip'] for camera in camera_data if camera['type'] == 'exterior']
+all_cameras = [camera['ip'] for camera in camera_data]
+
+# load environment
+load_dotenv()
+
+# constants
+SYSTEM_ARMED_PIN = 16
+SYSTEM_ALARM_PIN = 20
+LOGFILE_PATH = os.environ['LOGFILE_PATH']
+logger = logging.getLogger(__name__)
+logging.basicConfig(format='%(asctime)s [%(levelname)s] %(message)s', filename=LOGFILE_PATH,
+                    level=os.environ['LOG_DEBUG'])
+
+"""
+ Functions used within the main loop of the program.
+"""
+
+
+def privacy_api_calls(camera_ips=None, status=False):
+    """
+    :param camera_ips: A list of camera IP addresses. Defaults to an empty list.
+    :param status: A boolean indicating whether to enable or disable the privacy mask. Defaults to False.
+    :return: None
+    """
+    for ip in camera_ips:
+        for i in range(4):
+            encode_blend = str(int(status))  # Convert to 1 or 0 (True/False)
+            url = f'http://{ip}/cgi-bin/configManager.cgi?action=setConfig&VideoWidget[0].Covers[{i}].EncodeBlend={encode_blend}'
+            response = requests.get(url,
+                                    auth=HTTPDigestAuth(os.environ['CAMERA_USERNAME'], os.environ['CAMERA_PASSWORD']))
+
+            if response.status_code != 200:
+                logger.error(f"Request to {ip} returned status code {response.status_code}.")
+
+
+def send_email(subject, body):
+    postmark = PostmarkClient(server_token=os.environ['POSTMARK_API'])
+    postmark.emails.send(
+        From=os.environ['FROM_ADDRESS'],
+        To=os.environ['TO_ADDRESS'],
+        Subject=f'[{platform.node()}] {subject}',
+        TextBody=body,
+    )
+
+
+def log_current_state():
+    logger.info('System Armed: ' + str(last_armed_state))
+    logger.info('System Alarm: ' + str(last_alarm_state))
+
+
+def update_privacy_masks():
+    if last_armed_state and not last_alarm_state:
+        print("Interior privacy masks off")
+        log_current_state()
+        logger.info('Interior privacy masks off')
+        send_email(subject='Interior privacy masks off', body='Privacy change')
+        #privacy_api_calls(camera_ips=interior_cameras)
+    elif last_alarm_state:
+        print("All privacy masks off")
+        log_current_state()
+        logger.info('All privacy masks off')
+        send_email(subject='All privacy masks off', body='Privacy change')
+        #privacy_api_calls(camera_ips=all_cameras)
+    else:
+        print("Privacy masks on")
+        log_current_state()
+        logger.info('Privacy masks on')
+        send_email(subject='Privacy masks on', body='Privacy change')
+        #privacy_api_calls(camera_ips=all_cameras, status=True)
+
+
+"""
+    Main Program
+"""
+send_email('Controller booted', datetime.now().strftime('%c') + ': Powered on')
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(SYSTEM_ARMED_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(SYSTEM_ALARM_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+# Get the current state
+logger.info('GPIO configured')
+last_armed_state = GPIO.input(SYSTEM_ARMED_PIN)
+last_alarm_state = GPIO.input(SYSTEM_ALARM_PIN)
+
+# send an initial email and set the initial state for privacy masks
+update_privacy_masks()
+
+try:
+    while True:
+        time.sleep(1)
+        current_armed_stated = GPIO.input(SYSTEM_ARMED_PIN)
+        current_alarm_stated = GPIO.input(SYSTEM_ALARM_PIN)
+
+        if current_armed_stated != last_armed_state:
+            last_armed_state = current_armed_stated
+            update_privacy_masks()
+        if current_alarm_stated != last_alarm_state:
+            last_alarm_state = current_alarm_stated
+            update_privacy_masks()
+
+except KeyboardInterrupt:
+    GPIO.cleanup()
